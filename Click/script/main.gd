478ce5d9c55b9441737d0d0675cfc94caf38bf43
Atamert Ölçgen <muhuk@muhuk.com>
2017-12-02 16:51:44 +0800

    Insert donut on click

--- Click/script/main.gd
+++ Click/script/main.gd
@@ -1,13 +1,22 @@
 extends Node2D
 
 
+const DONUT = preload("res://Click/scene/Donut.tscn")
+
+
 func _ready():
 	set_process_unhandled_input(true)
 	pass
 
 
 func _unhandled_input(event):
-	# Exit game when ESCAPE is pressed.
 	if event.type == InputEvent.KEY and !event.pressed and event.scancode == KEY_ESCAPE:
+		# Exit game when ESCAPE is pressed.
 		self.get_tree().quit()
+	elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == 1:
+		# Left click
+		var d = DONUT.instance()
+		d.set_scale(Vector2(10, 10))
+		d.set_pos(event.pos)
+		add_child(d)
 	pass
