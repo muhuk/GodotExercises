08f839165deffed23930b05ae4eee3772b8f0d9d
Atamert Ölçgen <muhuk@muhuk.com>
2017-12-02 11:37:23 +0800

    Draw the circle

--- Click/script/Circle.gd
+++ Click/script/Circle.gd
@@ -5,11 +5,21 @@ extends Polygon2D
 # var b = "textvar"
 
 const VERTEXT_COUNT = 32
+const TWO_PI = 2 * PI
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	var polygon = Vector2Array([Vector2(0, 0), Vector2(10, 0), Vector2(10, 10), Vector2(0, 10)])
-	self.set_polygon(polygon)
-	self.set_pos(Vector2(10, 10))
+	self.set_polygon(calculate_circle_vertices(VERTEXT_COUNT, 50))
+	self.set_pos(Vector2(100, 100))
 	pass
+
+
+func calculate_circle_vertices(n, r):
+	var arr = Vector2Array()
+	for i in range(n):
+		var k = float(i) / n
+		var v = Vector2(cos(k * TWO_PI) * r, sin(k * TWO_PI) * r)
+		arr.append(v)
+	print(arr)
+	return arr
