409b71754acfd627ed6bf4d28142d9d4a0f8bd9f
Atamert Ölçgen <muhuk@muhuk.com>
2017-12-02 11:44:05 +0800

    Make vertex count & radius editable in the editor

--- Click/script/Circle.gd
+++ Click/script/Circle.gd
@@ -1,16 +1,14 @@
 extends Polygon2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-const VERTEXT_COUNT = 32
 const TWO_PI = 2 * PI
+
+export(int) var vertex_count = 32
+export(float) var radius = 10
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	self.set_polygon(calculate_circle_vertices(VERTEXT_COUNT, 50))
+	self.set_polygon(calculate_circle_vertices(vertex_count, radius))
 	self.set_pos(Vector2(100, 100))
 	pass
 
@@ -21,5 +19,5 @@ func calculate_circle_vertices(n, r):
 		var k = float(i) / n
 		var v = Vector2(cos(k * TWO_PI) * r, sin(k * TWO_PI) * r)
 		arr.append(v)
-	print(arr)
+
 	return arr
